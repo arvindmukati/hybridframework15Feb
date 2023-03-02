0e43b027ce25c06ac7a91811ac18cbf079a82476
arvindmukati <arvindmukatee@gmail.com>
2023-03-02 12:30:16 +0530

    Added fixture with function scope and autouse for bowser launch

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,23 +1,25 @@
 from selenium import webdriver
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
+import pytest
 
 
 class TestLoginUI:
+    @pytest.fixture(scope="function",autouse=True)
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        yield
+        self.driver.quit()
+
     def test_title(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        actual_title = driver.title
+        actual_title = self.driver.title
         assert_that("OrangeHRM").is_equal_to(actual_title)
         #assert actual_title == "OrangeHRM"
 
     def test_header(self):
-        driver=webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        text = driver.find_element(By.XPATH,"//h5[@class='oxd-text oxd-text--h5 orangehrm-login-title']").text
+        text = self.driver.find_element(By.XPATH,"//h5[@class='oxd-text oxd-text--h5 orangehrm-login-title']").text
         assert_that("Login").is_equal_to(text)
 
